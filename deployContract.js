028c2e9984e278f557acc3be02554eec4213e1b4
rachinkapoor <rachin@ost.com>
2018-10-02 01:23:24 +0530

    comment the code a bit.

--- deployContract.js
+++ deployContract.js
@@ -1,26 +1,32 @@
 'use strict';
 
+/**
+ * @fileoverview Nodejs Program to deploy ethereum smart contracts. See deploy method for sample code.
+ * @author rachin@ost.com (Rachin Kapoor)
+ */
+
 const fs = require('fs');
 const path = require('path');
 const OpenST = require('@openstfoundation/openst.js');
 
 class Performer {
-  constructor(configPath, jsonInterfacePath, byteCodePath, contractArgs) {
+  constructor(config, jsonInterfacePath, byteCodePath, contractArgs) {
     this.jsonInterface = this.buildAbi(jsonInterfacePath);
     this.byteCode = this.buildBin(byteCodePath);
 
-    let config = this.getSetupConfig(configPath),
-      provider = config.gethRpcEndPoint;
+    let provider = config.gethRpcEndPoint;
 
-    //Create Object of openst.js and access web3 object.
+    //Create Object of openst.js.
     let openSt = new OpenST(provider);
-    let web3 = (this.web3 = openSt.web3());
+    let web3 = openSt.web3();
 
     //Add Geth Signer Service so that we can unlock and sign using deployerAddress.
     let gethSigner = new openSt.utils.GethSignerService(web3);
     let passphrase = 'testtest';
     gethSigner.addAccount(config.deployerAddress, passphrase);
     openSt.signers.setSignerService(gethSigner);
+
+    this.openSt = openSt;
 
     //Set the deployer params.
     this.deployParams = {
@@ -33,7 +39,13 @@ class Performer {
   }
 
   deploy() {
-    let contract = new this.web3.eth.Contract(this.jsonInterface);
+    //1. Get web3 object from openst.
+    let web3 = this.openSt.web3();
+
+    //2. Create an instance of contract.
+    let contract = new web3.eth.Contract(this.jsonInterface);
+
+    //3. Deploy the contract.
     try {
       contract
         .deploy({
@@ -58,80 +70,6 @@ class Performer {
     }
   }
 
-  static logReceipt(receipt) {
-    let message = JSON.stringify(receipt, null, 1);
-
-    if (receipt.status) {
-      console.log(
-        '\n\n',
-        '===========================\x1b[32m Transaction Successful \x1b[0m===========================',
-        '\n\n'
-      );
-    } else {
-      console.log(
-        '\n\n',
-        '==============================\x1b[31m Transaction Failed \x1b[0m============================',
-        '\n\n'
-      );
-    }
-    console.log('\x1b[2m', message, '\x1b[0m');
-  }
-
-  static logError(message) {
-    console.error('\x1b[0m', '\x1b[31m', message, '\x1b[0m');
-  }
-
-  static logSuccess(subject, message) {
-    console.info('\x1b[0m', '\x1b[32m', subject, '\x1b[0m', '\x1b[34m', message, '\x1b[0m');
-  }
-
-  static exitWithError(err) {
-    if (err) {
-      console.log(
-        '\n\n',
-        '==============================\x1b[31m ERROR \x1b[0m=======================================',
-        '\n\n'
-      );
-      Performer.logError(typeof err === 'string' ? err : err.message);
-      console.log('\n\n', '============================== END-OF-PROGRAM ==============================', '\n\n');
-    }
-
-    process.exit(1);
-  }
-
-  static exitWithoutError(subject, message) {
-    if (subject || message) {
-      console.log(
-        '\n\n',
-        '==============================\x1b[32m SUCCESS \x1b[0m=====================================',
-        '\n\n'
-      );
-      Performer.logSuccess(subject, message);
-      console.log(
-        '\n\n',
-        '==============================\x1b[34m END-OF-PROGRAM \x1b[0m==============================',
-        '\n\n'
-      );
-    }
-    process.exit(0);
-  }
-
-  getSetupConfig(configInPath) {
-    let configPath = null;
-    try {
-      configPath = path.resolve(configInPath);
-      return require(configPath);
-    } catch (e) {
-      Performer.logError(e);
-      let error = new Error(
-        'Invalid Config File Path: ' +
-          (configPath || configInPath) +
-          '\nPlease provide openst-setup/config.json path using -c or --config flag'
-      );
-      Performer.exitWithError(error);
-    }
-  }
-
   buildAbi(abiInPath) {
     //Validate abiPath
     let abiPath = null;
@@ -142,9 +80,7 @@ class Performer {
       return abi;
     } catch (e) {
       Performer.logError(e);
-      let error = new Error(
-        'Invalid ABI Path: ' + (abiPath || abiInPath) + '\nPlease provide contract Abi using -a or --abi flag'
-      );
+      let error = new Error('Invalid ABI Path: ' + (abiPath || abiInPath) + '\nPlease provide contract Abi using -a or --abi flag');
       Performer.exitWithError(error);
     }
   }
@@ -163,17 +99,101 @@ class Performer {
       return binContent;
     } catch (e) {
       Performer.logError(e);
+      let error = new Error('Invalid BIN Path: ' + (binPath || binInPath) + '\nPlease provide contract Bin using -b or --bin flag');
+      Performer.exitWithError(error);
+    }
+  }
+
+  /**
+   * getSetupConfig() returns the openst-setup config object
+   * based on passed config file path.
+   *
+   * @param {string} configInPath - Required. Relative or absolute Path to config.json.
+   * @return {object} openst-setup config object.
+   */
+  static getSetupConfig(configInPath) {
+    let configPath = null;
+    try {
+      configPath = path.resolve(configInPath);
+      return require(configPath);
+    } catch (e) {
+      Performer.logError(e);
       let error = new Error(
-        'Invalid BIN Path: ' + (binPath || binInPath) + '\nPlease provide contract Bin using -b or --bin flag'
+        'Invalid Config File Path: ' + (configPath || configInPath) + '\nPlease provide openst-setup/config.json path using -c or --config flag'
       );
       Performer.exitWithError(error);
     }
   }
+
+  /**
+   * logReceipt() logs etherium transaction receipt
+   *
+   * @param {object} receipt - Required. Etherium Transaction Receipt to be logged.
+   */
+  static logReceipt(receipt) {
+    let message = JSON.stringify(receipt, null, 1);
+
+    if (receipt.status) {
+      console.log('\n\n', '=====\x1b[32m Transaction Successful \x1b[0m=====', '\n\n');
+    } else {
+      console.log('\n\n', '=====\x1b[31m Transaction Failed \x1b[0m=====', '\n\n');
+    }
+    console.log('\x1b[2m', message, '\x1b[0m');
+  }
+
+  /**
+   * logError() logs an error message.
+   *
+   * @param {string} message - Message to be logged.
+   */
+  static logError(message) {
+    console.error('\x1b[0m', '\x1b[31m', message, '\x1b[0m');
+  }
+
+  /**
+   * logSuccess() logs success message.
+   m
+   * @param {string} subject - Subject of message to be logged.
+   * @param {string} message - Success message to be logged.
+   */
+  static logSuccess(subject, message) {
+    console.info('\x1b[0m', '\x1b[32m', subject, '\x1b[0m', '\x1b[34m', message, '\x1b[0m');
+  }
+
+  /**
+   * exitWithError() logs error message and exits the program.
+   *
+   * @param {string|object} error - Error object or error message.
+   */
+  static exitWithError(error) {
+    if (err) {
+      console.log('\n\n', '==========\x1b[31m ERROR \x1b[0m==========', '\n\n');
+      Performer.logError(typeof error === 'string' ? error : error.message);
+      console.log('\n\n', '==========\x1b[31m END-OF-PROGRAM \x1b[0m==========', '\n\n');
+    }
+
+    process.exit(1);
+  }
+
+  /**
+   * exitWithError() logs success message and exits the program.
+   *
+   * @param {string} subject - Subject of message to be logged.
+   * @param {string} message - Success message to be logged.
+   */
+  static exitWithoutError(subject, message) {
+    if (subject || message) {
+      console.log('\n\n', '==========\x1b[32m SUCCESS \x1b[0m==========', '\n\n');
+      Performer.logSuccess(subject, message);
+      console.log('\n\n', '==========\x1b[34m END-OF-PROGRAM \x1b[0m==========', '\n\n');
+    }
+    process.exit(0);
+  }
 }
 
 const program = require('commander')
-  .option('-a, --abi <file>')
-  .option('-b, --bin <file>')
+  .option('-a, --abi <file>', 'Required. Path to smart-contract Abi file.')
+  .option('-b, --bin <file>', 'Required. Path to smart-contract bin file.')
   .option('-c, --config <path to openst-setup folder>', 'defaults to ./openst-setup/config.json');
 
 program.parse(process.argv);
@@ -189,5 +209,7 @@ let contractArgs = program.args || [],
   abi,
   bin;
 
-let performer = new Performer(configPath, abiInPath, binInPath, contractArgs);
+let config = Performer.getSetupConfig(configPath),
+  performer = new Performer(config, abiInPath, binInPath, contractArgs);
+
 performer.deploy();
