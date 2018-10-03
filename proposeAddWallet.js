8d31b12d1b21e4cba76d727853219b1a3cc04d29
Kedar Chandrayan <kedarchandrayan@gmail.com>
2018-10-03 16:12:47 +0530

    propose add wallet.

--- proposeAddWallet.js
+++ proposeAddWallet.js
@@ -1,52 +1,73 @@
 'use strict';
 
-const program = require('commander');
+/**
+ * @fileoverview Nodejs Program to propose add wallet in a given TokenHolder Contract. See propose method for sample code.
+ * Contract: https://github.com/OpenSTFoundation/openst-contracts/blob/develop/contracts/TokenHolder.sol
+ * @author kedar@ost.com (Kedar Chandrayan)
+ */
 
+const PerformerBase = require('./PerformerBase');
+
+class Performer extends PerformerBase {
+  constructor(program) {
+    super(program);
+
+    this.tokenHolderAddress = program.tokenHolderAddress;
+    this.walletToPropose = program.walletToPropose;
+    this.wallet = program.wallet;
+
+    //Validate the inputs.
+    this.validate();
+  }
+
+  propose() {
+    let openST = this.openST;
+    let config = this.getSetupConfig();
+    let tokenHolder = new openST.contracts.TokenHolder(this.tokenHolderAddress);
+    // Propose a new wallet by calling submitAddWallet
+    tokenHolder
+      .submitAddWallet(this.walletToPropose)
+      .send({
+        from: this.wallet,
+        gasPrice: config.gasPrice,
+        gas: config.gas
+      })
+      .then((receipt) => {
+        this.logReceipt(receipt);
+        if (receipt.status && receipt.events.WalletAdditionSubmitted) {
+          this.logSuccess('WalletAdditionSubmitted Event:', receipt.events.WalletAdditionSubmitted);
+          this.exitWithoutError('Transaction Id:', receipt.events.WalletAdditionSubmitted.returnValues._transactionID);
+        } else {
+          this.exitWithError('Failed to Confirm Transaction. See receipt for details.');
+        }
+      })
+      .catch((reason) => {
+        this.logError(reason);
+        this.exitWithError('Failed to Confirm Transaction. See error for details.');
+      });
+  }
+
+  validate() {}
+}
+
+const program = PerformerBase.getProgram();
 program
   .option('-t, --token-holder-address [tokenHolderAddress]', 'TokenHolder contract address')
   .option('-p, --wallet-to-propose [walletToPropose]', 'Wallet to propose')
-  .option('-w, --wallet [wallet]', 'Wallet address')
-  .parse(process.argv);
+  .option('-w, --wallet [wallet]', 'Wallet address');
 
-// TODO - validate here for the params
+program.on('--help', () => {
+  console.log('');
+  console.log('  Example:');
+  console.log('');
+  console.log(
+    '    node proposeAddWallet.js -t 0x3D7bb53A5d731B157554E32a6499162070365C06 -p 0xea674fdde714fd979de3edf0f56aa9716b898ec8 -w 0xe7817ce78558ca0e43f11a975acc6027eb845a5a'
+  );
+  console.log('');
+  console.log('');
+});
 
-let tokenHolderAddress = program.tokenHolderAddress,
-  walletToPropose = program.walletToPropose,
-  wallet = program.wallet;
+program.parse(process.argv);
 
-const os = require('os');
-let configFilePath = os.homedir() + '/openst-setup/config.json';
-
-let config = require(configFilePath),
-  provider = config.gethRpcEndPoint,
-  passphrase = 'testtest';
-
-const OpenST = require('@openstfoundation/openst.js');
-let openST = new OpenST(provider);
-
-let tokenHolder = new openST.contracts.TokenHolder(tokenHolderAddress);
-
-console.log('* submitAuthorizeSession from wallet:', wallet);
-
-let gethSigner = new openST.utils.GethSignerService(openST.web3());
-gethSigner.addAccount(wallet, passphrase);
-openST.signers.setSignerService(gethSigner);
-
-// Authorize an ephemeral public key
-tokenHolder
-  .submitAddWallet(walletToPropose)
-  .send({
-    from: wallet,
-    gasPrice: config.gasPrice,
-    gas: config.gas
-  })
-  .then(function(submitAddWalletReceipt) {
-    if (submitAddWalletReceipt.events.WalletAdditionSubmitted) {
-      console.log('submitAddWallet DONE!');
-
-      let transactionId = submitAddWalletReceipt.events.WalletAdditionSubmitted.returnValues._transactionId;
-      console.log('transactionId for submitAddWallet', transactionId);
-    } else {
-      console.log('Error in submitAddWallet.');
-    }
-  });
+let performer = new Performer(program);
+performer.propose();
