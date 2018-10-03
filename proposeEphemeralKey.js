c8a07f4879514ed682628687b7744ba605eb835a
rachinkapoor <rachin@ost.com>
2018-10-03 15:45:46 +0530

    put in example

--- proposeEphemeralKey.js
+++ proposeEphemeralKey.js
@@ -57,6 +57,17 @@ program
   .option('-x, --expiration-height [expirationHeight]', 'Expiration height')
   .option('-e, --ephemeral-key-address [ephemeralKeyAddress]', 'Ephemeral key address');
 
+program.on('--help', () => {
+  console.log('');
+  console.log('  Example:');
+  console.log('');
+  console.log(
+    '    node proposeEphemeralKey.js -t 0xf8c9018DEA785A7cc8A59fE1F8A64B6f64f060cD -w 0xbba2c47be3add4fd302d9a8122442ca9d65ad9a3 -s 5000000000000000000 -x 200000000 -e 0xee339904c3a947f0e5eac4e7e4d0da835321f7a0'
+  );
+  console.log('');
+  console.log('');
+});
+
 program.parse(process.argv);
 
 let performer = new Performer(program);
